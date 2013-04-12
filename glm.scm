e0a71814a6e868de2d2ebfd54dd4d96b1d2e0126
Kristian Lein-Mathisen <kristianlein@gmail.com>
2013-04-12 21:50:22 +0200

    feat: adds print-mat4
    we're going to need prints for the other mats too

--- glm.scm
+++ glm.scm
@@ -229,6 +229,24 @@
  (define (transpose/T mat)  (with-destination (make-T #f) transpose/T! mat)))
 
 
+(define (print-mat4 mat #!optional
+                    (preline (lambda () (void)))
+                    (postline print)
+                    (precell (lambda () (void)))
+                    (postcell (lambda () (display '#\space))))
+  (do ((i 0 (add1 i)))
+      ((>= i 4))
+    (preline)
+    (do ((j 0 (add1 j)))
+        ((>= j 4))
+      (precell)
+      (display (f32vector-ref mat (+ (* i 4) j)))
+      (postcell))
+    (postline)))
+
+
+
+
 ;; lookat cross translate rotate degrees radians
 ;; vec * scalar
 ;; vec * vec
